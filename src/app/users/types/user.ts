98ff3b7414cc738802b5b16d1372c17ef9e2a3f2
RogerJackD <rogerzapata114@gmail.com>
2025-09-30 11:54:44 -0500

    add types userResponse && User && Error

---
+++ src/app/users/types/user.ts
@@ -0,0 +1,24 @@
+export interface CreateUserDto {
+    name: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    address: string;
+}
+
+export interface User {
+    id: string;
+    name: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    address: string;
+    status: string;
+    createdAt: string;
+}
+
+export interface ValidationErrors {
+    message: string;
+    error: string;
+    statusCode: number;
+}

24cc2cbc6ca65b6d66ca02dd07deebfb0fa5dc82
RogerJackD <rogerzapata114@gmail.com>
2025-09-30 22:50:16 -0500

    add new fields validations

--- src/lib/validations/form.ts
+++ src/lib/validations/form.ts
@@ -2,12 +2,22 @@ import { z } from "zod";
 
 
 export const formSchema = z.object({
-  userName: z.string().min(2, { message: "min 2 characters" }),
+  name: z.
+    string()
+    .min(2, { message: "Minimo dos caracteres" }),
+  lastName: z.
+    string().
+    optional(),
   email: z
     .string()
     .min(1, { message: "Email is required" })
-    .email({ message: "email invalido" }),
-  password: z
+    .email({ message: "Email invalido" }),
+  phone: z
     .string()
-    .min(6, { message: "the password must have min 6 characters" }),
+    .regex(/^9\d{8}$/, {
+      message: "El número debe tener 9 dígitos y comenzar con 9 (ej: 912345678).",
+    }),
+  address: z
+    .string()
+    .optional()
 });
